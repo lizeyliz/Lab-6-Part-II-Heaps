0ee784a58ecc704403bbf4f3ff6daa6a9d2723f5
sarsparvier <sarsparvier@gmail.com>
2025-03-07 09:43:53 -0800

    finished patient class

--- Patient.java
+++ Patient.java
@@ -14,4 +14,54 @@ class Patient {
         this.admitTime = admitTime;
         this.priorityLevel = priorityLevel;
     }//end constructor
+
+    //getters
+
+    public String getName() {
+        return name;
+    }//end getName
+
+    public int getAge() {
+        return age;
+    }//end getAge
+
+    public boolean getInCritCondition() {
+        return inCritCondition;
+    }//end getInCritCondition
+
+    public int getAdmitTime() {
+        return admitTime;
+    }//end getAdmitTime
+
+    public int getPriorityLevel() {
+        return priorityLevel;
+    }//end getPriorityLevel
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }//end setName
+
+    public void setAge(int age) {
+        this.age = age;
+    }//end setAge
+
+    public void setInCritCondition(boolean inCritCondition) {
+        this.inCritCondition = inCritCondition;
+    }//end setInCritCondition
+
+    public void setAdmitTime(int admitTime) {
+        this.admitTime = admitTime;
+    }//end setAdmitTime
+
+    public void setPriorityLevel(int priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }//end setPriorityLevel
+
+    //toString
+    public String toString() {
+        return "Name: " + name + "\nAge: " + age + "\nCritical Condition: " + inCritCondition + "\nAdmit Time: " + admitTime + "\nPriority Level: " + priorityLevel;
+    }//end toString
+
+    
 }//end class patient
