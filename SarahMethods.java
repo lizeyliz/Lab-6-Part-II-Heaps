6f4b59154d888f8ce8a4c7daab70e2c158b065b3
sarsparvier <sarsparvier@gmail.com>
2025-03-09 14:55:12 -0700

    created swap methods

--- SarahMethods.java
+++ SarahMethods.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SarahMethods {
     Scanner input = new Scanner(System.in);
+    private ArrayList<Patient> patients = new ArrayList<>(); 
 
     public void updatePatientStatus(Patient patient) {
         System.out.println("What is the patient's new status?");
@@ -11,7 +13,21 @@ public class SarahMethods {
 
     }
 
-    
+    public void swap(int index1, int index2){
+        if (index1 >= 0 && index1 < patients.size() && index2 >= 0 && index2 < patients.size()) {
+            //swap the two patients in the min heap
+            Patient temp = patients.get(index1);
+            patients.set(index1, patients.get(index2));
+            patients.set(index2, temp);
+
+            //System.out.println(patients.get(index1));
+            //System.out.println(patients.get(index2));
+        } else {
+            System.out.println("Error: Index out of bounds");
+        }
+    }
+
+
     
 
     
