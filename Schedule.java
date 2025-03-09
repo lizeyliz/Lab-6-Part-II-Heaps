aa935d387a5f890090b43e4c987b04fa4d1950ca
lizeyliz <alizaeaton@gmail.com>
2025-03-09 13:49:52 -0700

    starting schedule class

--- Schedule.java
+++ Schedule.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
 public class Schedule {
-    
-}
+    private Boolean[] openRooms = new Boolean [10];//true if room open, false if room occupied
+    private ArrayList<Patient> patients = new ArrayList<>(); //min heap of patients (most urgent on top)
+    //one open room for super urgent patients
+
+    //what to do if several urgent patients arrive at once?
+
+    //constructor
+    public Schedule() {
+        this.patients = new ArrayList<>(); //initalize empty ArrayList
+    }//end constructor
+
+}//end Schedule class
